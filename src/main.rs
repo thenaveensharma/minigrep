2091b0845bbd31689c276498a8e2be822e92f501
Naveen Kumar <naveen.kumar@ronianalytics.com>
2024-08-12 00:34:19 +0530

    learn. generics and how compilar works with generics

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
-use std::{fs::File, io::ErrorKind};
+struct Coordinates<T> {
+    x: T,
+    _y: T,
+}
+
+impl Coordinates<i32> {
+    fn distance_from_origin(&self) -> i32 {
+        self.x.pow(2) + self._y.pow(2)
+    }
+}
 fn main() {
-    let greeting_file_result = File::open("hello.txt");
-    let _greeting_file = match greeting_file_result {
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create("hello.txt") {
-                Ok(fc) => fc,
-                Err(e) => panic!("Problem creating the file: {e:?}"),
-            },
-            other_error => {
-                panic!("Problem opening the file {other_error:?}")
-            }
-        },
-    };
+    let point1 = Coordinates { x: 1, _y: 2 };
+    println!(
+        "Point1 x is {} and distance from origin is {}",
+        point1.x,
+        point1.distance_from_origin()
+    );
+    let integer = Some(1);
+    let integer = Some(1.0);
 }

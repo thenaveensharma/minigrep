23086fac0f83ed4077e862caa94c01856c5338a6
the naveen sharma <naveenchaurasiya1501@gmail.com>
2024-08-15 23:22:14 +0530

    learn. generic lifetimes in functions

--- src/main.rs
+++ src/main.rs
@@ -81,11 +81,27 @@
 
 // preventind dangling references with lifetimes
 
+// fn main() {
+//     let r;
+//     {
+//         let x = 5;
+//         r = &x;
+//     }
+//     print!("r: {r}");
+// }
+
+//generic lifetimes in functions
 fn main() {
-    let r;
-    {
-        let x = 5;
-        r = &x;
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+    let result = longest(string1.as_str(), string2);
+    print!("The longest string is {result}");
+}
+
+fn longest<'a>(str1: &'a str, str2: &'a str) -> &'a str {
+    if str1.len() > str2.len() {
+        str1
+    } else {
+        str2
     }
-    print!("r: {r}");
 }

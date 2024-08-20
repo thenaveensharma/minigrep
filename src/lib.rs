95fa31c7cc71b0cce87a660230797263653d9a38
the naveen sharma <naveenchaurasiya1501@gmail.com>
2024-08-20 22:32:06 +0530

    test with Result enum

--- src/lib.rs
+++ src/lib.rs
@@ -1,91 +1,110 @@
-use std::error::Error;
-use std::{env, fs};
+// pub fn add(left:usize,right:usize)->usize{
+//     left+right
+// }
 
-pub struct Config {
-    pub query: String,
-    pub file_path: String,
-    pub ignore_case: bool,
+// #[cfg(test)]
+// mod tests{
+//     use super::*;
+
+//     #[test]
+//     fn exploration(){
+//         let result=add(2, 4);
+//         assert_eq!(result,6);
+//     }
+
+//     #[test]
+//     fn another(){
+//         panic!("Make this test fail");
+//     }
+// }
+pub fn add_two(a: i32) -> i32 {
+    a + 2
 }
 
-impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
+#[derive(Debug, PartialEq)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+pub fn greeting(name: &str) -> String {
+    format!("Hello! {name}")
+}
+pub struct Guess {
+    value: i32,
+}
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 {
+            panic!("Guess value must be less than or equal to 100, got {value}.");
+        } else if value > 100 {
+            panic!("Guess value must be greater than or equal to 1, got {value}.");
         }
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-        let ignore_case = env::var("INGORE_CASE").is_ok();
-
-        Ok(Config {
-            query,
-            file_path,
-            ignore_case,
-        })
+        Guess { value }
     }
 }
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-
-    let results = if config.ignore_case {
-        search_case_insensitive(&config.query, &contents)
-    } else {
-        search(&config.query, &contents)
-    };
-
-    for line in results {
-        println!("{line}");
-    }
-    Ok(())
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
+
     #[test]
-    fn case_sensitive() {
-        let query = "duct";
-        let contents = "\
-Rust:
-safe, fast, productive.
-Pick three.
-Duct tape.";
-        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 10,
+            height: 20,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 4,
+        };
+        assert!(larger.can_hold(&smaller));
     }
+
     #[test]
-    fn case_insensitive() {
-        let query = "RuSt";
-        let contents = "\
-Rust:
-safe, fast, productive.
-Pick three.
-Trust me.";
-        assert_eq!(
-            vec!["Rust:", "Trust me."],
-            search_case_insensitive(query, contents)
+    fn smaller_can_not_hold_larger() {
+        let larger = Rectangle {
+            width: 10,
+            height: 20,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 4,
+        };
+        assert!(!smaller.can_hold(&larger));
+    }
+
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(add_two(2), 4);
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Naveen");
+        assert!(
+            result.contains("Naveen"),
+            "Greeting did not contain name, value was '{}'",
+            result
         );
     }
-}
+    #[test]
+    #[should_panic = "less than or equal to 100"]
+    fn greater_than_100() {
+        Guess::new(101);
+    }
 
-pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut result = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            result.push(line);
+    #[test]
+    fn it_works()->Result<(),String>{
+        if 2+2==4{
+            Ok(())
+        }else {
+            Err(String::from("two plus two does not equal four"))
         }
     }
 
-    result
-}
-pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let query = query.to_lowercase();
-    let mut result = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            result.push(line);
-        }
-    }
-
-    result
 }

c1af533a8caf382a83701fa878a810557d0e4f08
the naveen sharma <naveenchaurasiya1501@gmail.com>
2024-09-01 00:58:05 +0530

    change to unique name

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::process;
 
-use minigrep::Config;
+use naveengrep::Config;
 
 fn main() {
     let config = Config::build(env::args()).unwrap_or_else(|err| {
@@ -9,7 +9,7 @@ fn main() {
         process::exit(1);
     });
 
-    if let Err(e) = minigrep::run(config) {
+    if let Err(e) = naveengrep::run(config) {
         eprintln!("Application error: {e}");
         process::exit(1);
     }

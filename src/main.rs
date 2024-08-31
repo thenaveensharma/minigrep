a77c5585c407956ac90fcc0cb84a8b5f483eab47
the naveen sharma <naveenchaurasiya1501@gmail.com>
2024-08-31 22:59:31 +0530

    learn. closure

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,71 @@
-use std::env;
-use std::process;
+// use std::env;
+// use std::process;
 
-use minigrep::Config;
+// use minigrep::Config;
+
+// fn main() {
+//     let args: Vec<String> = env::args().collect();
+
+//     let config = Config::build(&args).unwrap_or_else(|err| {
+//         eprintln!("Problem parsing arguments: {err}");
+//         process::exit(1);
+//     });
+
+//     if let Err(e) = minigrep::run(config) {
+//         eprintln!("Application error: {e}");
+//         process::exit(1);
+//     }
+// }
+#[derive(Debug, PartialEq, Copy, Clone)]
+enum ShirtColor {
+    Red,
+    Blue,
+}
+struct Inventory {
+    shirts: Vec<ShirtColor>,
+}
+
+impl Inventory {
+    fn giveaway(&self, user_preference: Option<ShirtColor>) -> ShirtColor {
+        user_preference.unwrap_or_else(|| self.most_stocked())
+    }
+    fn most_stocked(&self) -> ShirtColor {
+        let mut num_red = 0;
+        let mut num_blue = 0;
+        for color in &self.shirts {
+            match color {
+                ShirtColor::Blue => num_blue += 1,
+                ShirtColor::Red => num_red += 1,
+            }
+        }
+        if num_red > num_blue {
+            ShirtColor::Red
+        } else {
+            ShirtColor::Blue
+        }
+    }
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let store = Inventory {
+        shirts: vec![ShirtColor::Red, ShirtColor::Blue, ShirtColor::Blue],
+    };
+    let user_pref1 = Some(ShirtColor::Red);
+    let giveaway1 = store.giveaway(user_pref1);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref1, giveaway1
+    );
+    let user_pref2 = None;
+    let giveaway2 = store.giveaway(user_pref2);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref2, giveaway2
+    );
 
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {err}");
-        process::exit(1);
-    });
-
-    if let Err(e) = minigrep::run(config) {
-        eprintln!("Application error: {e}");
-        process::exit(1);
-    }
+    let list = vec![1, 2, 3];
+    print!("Before defining closure: {list:?}");
+    let only_borrows = || println!("From closure : {list:?}");
+    only_borrows();
+    print!("After defining closure: {list:?}");
 }

4605c7f180c323b9abd401d9732065fe638fc00d
the naveen sharma <naveenchaurasiya1501@gmail.com>
2024-08-31 21:55:55 +0530

    restore project

--- src/lib.rs
+++ src/lib.rs
@@ -1,125 +1,106 @@
-// pub fn add(left:usize,right:usize)->usize{
-//     left+right
-// }
+use std::env;
+// --snip--
 
-// #[cfg(test)]
-// mod tests{
-//     use super::*;
+use std::error::Error;
+use std::fs;
 
-//     #[test]
-//     fn exploration(){
-//         let result=add(2, 4);
-//         assert_eq!(result,6);
-//     }
-
-//     #[test]
-//     fn another(){
-//         panic!("Make this test fail");
-//     }
-// }
-pub fn add_two(a: i32) -> i32 {
-    a + 2
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+    pub ignore_case: bool,
 }
 
-#[derive(Debug, PartialEq)]
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
-impl Rectangle {
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
-    }
-}
-pub fn greeting(name: &str) -> String {
-    format!("Hello! {name}")
-}
-pub struct Guess {
-    value: i32,
-}
-impl Guess {
-    pub fn new(value: i32) -> Guess {
-        if value < 1 {
-            panic!("Guess value must be grater than or equal to 1, got {value}.");
-        } else if value > 100 {
-            panic!("Guess value must be less than or equal to 100, got {value}.");
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
         }
 
-        Guess { value }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
-fn prints_and_returns_10(a: i32) -> i32 {
-    println!("I got the value {a}");
-    10
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{line}");
+    }
+
+    Ok(())
 }
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+pub fn search_case_insensitive<'a>(
+    query: &str,
+    contents: &'a str,
+) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn this_test_will_pass(){
-        let value= prints_and_returns_10(4);
-        assert_eq!(10,value);
-    }
-    #[test]
-    #[ignore = "my choice"]
-    fn this_test_will_pass_too(){
-        let value= prints_and_returns_10(8);
-        assert_eq!(10,value);
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 
     #[test]
-    fn larger_can_hold_smaller() {
-        let larger = Rectangle {
-            width: 10,
-            height: 20,
-        };
-        let smaller = Rectangle {
-            width: 5,
-            height: 4,
-        };
-        assert!(larger.can_hold(&smaller));
-    }
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
 
-    #[test]
-    fn smaller_can_not_hold_larger() {
-        let larger = Rectangle {
-            width: 10,
-            height: 20,
-        };
-        let smaller = Rectangle {
-            width: 5,
-            height: 4,
-        };
-        assert!(!smaller.can_hold(&larger));
-    }
-
-    #[test]
-    fn it_adds_two() {
-        assert_eq!(add_two(2), 4);
-    }
-
-    #[test]
-    fn greeting_contains_name() {
-        let result = greeting("Naveen");
-        assert!(
-            result.contains("Naveen"),
-            "Greeting did not contain name, value was '{}'",
-            result
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
         );
-    }
-    #[test]
-    #[should_panic = "less than or equal to 100"]
-    fn greater_than_100() {
-        Guess::new(200);
-    }
-
-    #[test]
-    fn it_works() -> Result<(), String> {
-        if 2 + 2 == 4 {
-            Ok(())
-        } else {
-            Err(String::from("two plus two does not equal four"))
-        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,172 +1,18 @@
-// fn main() {
-//     let tweet = Tweet {
-//         username: String::from("the_naveen_sharma"),
-//         content: String::from("Follow me to learn rust"),
-//         reply: false,
-//         retweet: false,
-//     };
+use std::env;
+use std::process;
 
-//     print!("1 new tweet {}", tweet.summarize());
+use minigrep::Config;
 
-//     let article = NewsArticle {
-//         headline: String::from("Penguins win the Stanley Cup Championship!"),
-//         location: String::from("Pittsburgh, PA, USA"),
-//         author: String::from("Iceburgh"),
-//         content: String::from(
-//             "The Pittsburgh Penguins once again are the best \
-//              hockey team in the NHL.",
-//         ),
-//     };
+fn main() {
+    let args: Vec<String> = env::args().collect();
 
-//     println!("New article available! {}", article.summarize());
-//     notify(&article);
-//     let x = std::f64::consts::PI;
-//     let r = 8.0;
-//     println!("the area of the circle is {}", x * r * r);
-// }
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-// pub trait Summary {
-//     fn summarize_autor(&self) -> String;
-//     fn summarize(&self) -> String {
-//         format!("(Read more from {}...)", self.summarize_autor())
-//     }
-// }
-// pub struct NewsArticle {
-//     pub headline: String,
-//     pub location: String,
-//     pub author: String,
-//     pub content: String,
-// }
-
-// impl Summary for NewsArticle {
-//     fn summarize(&self) -> String {
-//         format!("{}, by {} ({})", self.headline, self.author, self.location)
-//     }
-
-//     fn summarize_autor(&self) -> String {
-//         todo!()
-//     }
-// }
-
-// pub struct Tweet {
-//     pub username: String,
-//     pub content: String,
-//     pub reply: bool,
-//     pub retweet: bool,
-// }
-// impl Summary for Tweet {
-//     fn summarize(&self) -> String {
-//         format!("{}: {}", self.username, self.content)
-//     }
-
-//     fn summarize_autor(&self) -> String {
-//         todo!()
-//     }
-// }
-
-// type Meters = u32;
-// impl Summary for Meters {
-//     fn summarize(&self) -> String {
-//         format!("{}", self)
-//     }
-
-//     fn summarize_autor(&self) -> String {
-//         todo!()
-//     }
-// }
-
-// pub fn notify<T:Summary>(item: &T) {
-//     println!("Breaking news! {}", item.summarize());
-// }
-
-// preventind dangling references with lifetimes
-
-// fn main() {
-//     let r;
-//     {
-//         let x = 5;
-//         r = &x;
-//     }
-//     print!("r: {r}");
-// }
-
-//generic lifetimes in functions
-// fn main() {
-//     let string1 = String::from("long string");
-//     {
-//         let string2 = "xyz";
-//         let result = longest(string1.as_str(), string2);
-//         print!("The longest string is {result}");
-//     }
-// }
-
-// fn longest<'a,'b>(str1: &'a str, str2: &'b str) -> & str {
-//     if str1.len() > str2.len() {
-//         str1
-//     } else {
-//         str2
-//     }
-// }
-
-// Lifetime annotations in struct definations
-
-// struct ImportantExcerpt<'a> {
-//     part: &'a str,
-// }
-// impl<'a> ImportantExcerpt<'a> {
-//     fn level(&self)->i32{
-//         3
-//     }
-//     fn announance_and_return_part(&self,announcement:&str)->&str{
-//         print!("Attention please: {announcement}");
-//         self.part
-//     }
-// }
-// fn main() {
-//     let novel = String::from("Call me Naveen. Some years ago...");
-//     let first_sentence = novel.split('.').next().unwrap();
-//     print!("{first_sentence}");
-//     let i = ImportantExcerpt {
-//         part: first_sentence,
-//     };
-// }
-
-// fn main() {
-// first_word("The Naveen Sharma");
-// }
-
-// fn first_word<'a>(s: &'a str) -> &'a str {
-//     let bytes = s.as_bytes();
-//     for (i, &val) in bytes.iter().enumerate() {
-//         if val == b' ' {
-//             return &s[0..i];
-//         }
-//     }
-//     &s[..]
-// }
-
-// use std::fmt::Display;
-
-// Generic Type Parameters, Trait Bounds, and Lifetimes Together
-// fn main() {
-//     let string1 = String::from("long string");
-//     {
-//         let string2 = "xyz";
-//         let result = longest_with_an_announcement(string1.as_str(), string2, "The Naveen");
-//         print!("The longest string is {result}");
-//     }
-// }
-
-// fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
-// where
-//     T: Display,
-// {
-//     println!("Announcemen! {ann}");
-//     if x.len() > y.len() {
-//         x
-//     } else {
-//         y
-//     }
-// }
-
-fn main() {}
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+}

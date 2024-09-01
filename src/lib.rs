8d343bf18776020e3e294c0a2c4c4f9776b953d7
the naveen sharma <naveenchaurasiya1501@gmail.com>
2024-09-01 23:09:36 +0530

    Smart pointers How to break recursive loop while implementing linked list

--- src/lib.rs
+++ src/lib.rs
@@ -23,7 +23,6 @@ pub struct Config {
 }
 
 impl Config {
-
     pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
         args.next();
 
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,26 @@
-use std::env;
-use std::process;
+// use std::env;
+// use std::process;
 
-use naveengrep::Config;
+// use naveengrep::Config;
 
-fn main() {
-    let config = Config::build(env::args()).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {err}");
-        process::exit(1);
-    });
+// fn main() {
+//     let config = Config::build(env::args()).unwrap_or_else(|err| {
+//         eprintln!("Problem parsing arguments: {err}");
+//         process::exit(1);
+//     });
 
-    if let Err(e) = naveengrep::run(config) {
-        eprintln!("Application error: {e}");
-        process::exit(1);
-    }
+//     if let Err(e) = naveengrep::run(config) {
+//         eprintln!("Application error: {e}");
+//         process::exit(1);
+//     }
+// }
+
+use crate::List::{Cons, Nil};
+
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
 }
-
-
-
+fn main() {
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+}

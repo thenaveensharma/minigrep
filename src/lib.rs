9c33ad33d9080cfc550d113ee28f539e38f68db6
the naveen sharma <naveenchaurasiya1501@gmail.com>
2024-09-07 14:43:16 +0530

    Interior Mutability: A Mutable Borrow to an Immutable Value

--- src/lib.rs
+++ src/lib.rs
@@ -1,114 +1,178 @@
-use std::env;
+// use std::env;
 
-use std::error::Error;
-use std::fs;
+// use std::error::Error;
+// use std::fs;
 
-/// Adds one to the number given.
-///
-/// # Examples
-///
-/// ```
-/// let arg = 5;
-/// let answer = my_crate::add_one(arg);
-///
-/// assert_eq!(6, answer);
-/// ```
-pub fn add_one(x: i32) -> i32 {
-    x + 1
+// /// Adds one to the number given.
+// ///
+// /// # Examples
+// ///
+// /// ```
+// /// let arg = 5;
+// /// let answer = my_crate::add_one(arg);
+// ///
+// /// assert_eq!(6, answer);
+// /// ```
+// pub fn add_one(x: i32) -> i32 {
+//     x + 1
+// }
+// pub struct Config {
+//     pub query: String,
+//     pub file_path: String,
+//     pub ignore_case: bool,
+// }
+
+// impl Config {
+//     pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+//         args.next();
+
+//         let query = match args.next() {
+//             Some(arg) => arg,
+//             None => return Err("Didn't get a query string"),
+//         };
+
+//         let file_path = match args.next() {
+//             Some(arg) => arg,
+//             None => return Err("Didn't get a file path"),
+//         };
+//         let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+//         Ok(Config {
+//             query,
+//             file_path,
+//             ignore_case,
+//         })
+//     }
+// }
+
+// pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+//     let contents = fs::read_to_string(config.file_path)?;
+
+//     let results = if config.ignore_case {
+//         search_case_insensitive(&config.query, &contents)
+//     } else {
+//         search(&config.query, &contents)
+//     };
+
+//     for line in results {
+//         println!("{line}");
+//     }
+
+//     Ok(())
+// }
+
+// pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+//     contents
+//         .lines()
+//         .filter(|line| line.contains(query))
+//         .collect()
+// }
+
+// pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+//     let query = query.to_lowercase();
+//     let mut results = Vec::new();
+
+//     for line in contents.lines() {
+//         if line.to_lowercase().contains(&query) {
+//             results.push(line);
+//         }
+//     }
+
+//     results
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn case_sensitive() {
+//         let query = "duct";
+//         let contents = "\
+// Rust:
+// safe, fast, productive.
+// Pick three.
+// Duct tape.";
+
+//         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+//     }
+
+//     #[test]
+//     fn case_insensitive() {
+//         let query = "rUsT";
+//         let contents = "\
+// Rust:
+// safe, fast, productive.
+// Pick three.
+// Trust me.";
+
+//         assert_eq!(
+//             vec!["Rust:", "Trust me."],
+//             search_case_insensitive(query, contents)
+//         );
+//     }
+// }
+
+pub trait Messenger {
+    fn send(&self, msg: &str);
 }
-pub struct Config {
-    pub query: String,
-    pub file_path: String,
-    pub ignore_case: bool,
+pub struct LimitTracker<'a, T: Messenger> {
+    messenger: &'a T,
+    value: usize,
+    max: usize,
 }
-
-impl Config {
-    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
-        args.next();
-
-        let query = match args.next() {
-            Some(arg) => arg,
-            None => return Err("Didn't get a query string"),
-        };
-
-        let file_path = match args.next() {
-            Some(arg) => arg,
-            None => return Err("Didn't get a file path"),
-        };
-        let ignore_case = env::var("IGNORE_CASE").is_ok();
-
-        Ok(Config {
-            query,
-            file_path,
-            ignore_case,
-        })
-    }
-}
-
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-
-    let results = if config.ignore_case {
-        search_case_insensitive(&config.query, &contents)
-    } else {
-        search(&config.query, &contents)
-    };
-
-    for line in results {
-        println!("{line}");
-    }
-
-    Ok(())
-}
-
-pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    contents
-        .lines()
-        .filter(|line| line.contains(query))
-        .collect()
-}
-
-pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let query = query.to_lowercase();
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
+impl<'a, T> LimitTracker<'a, T>
+where
+    T: Messenger,
+{
+    pub fn new(messenger: &'a T, max: usize) -> LimitTracker<'a, T> {
+        LimitTracker {
+            messenger,
+            value: 0,
+            max,
         }
     }
-
-    results
+    pub fn set_value(&mut self, value: usize) {
+        self.value = value;
+        let percentage_of_max = self.value as f64 / self.max as f64;
+        if percentage_of_max >= 1.0 {
+            self.messenger.send("Error: You are over your quota!");
+        } else if percentage_of_max >= 0.9 {
+            self.messenger
+                .send("Urgent warning: You've used upto over 90% of your quota!");
+        } else if percentage_of_max >= 0.75 {
+            self.messenger
+                .send("Warning: You've used up to over 75% of your qouta!");
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
+
     use super::*;
+    use std::cell::RefCell;
 
-    #[test]
-    fn case_sensitive() {
-        let query = "duct";
-        let contents = "\
-Rust:
-safe, fast, productive.
-Pick three.
-Duct tape.";
-
-        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    struct MockMessenger {
+        sent_messages: RefCell<Vec<String>>,
     }
-
+    impl MockMessenger {
+        fn new() -> MockMessenger {
+            MockMessenger {
+                sent_messages: RefCell::new(vec![]),
+            }
+        }
+    }
+    impl Messenger for MockMessenger {
+        fn send(&self, message: &str) {
+            self.sent_messages.borrow_mut().push(String::from(message));
+        }
+    }
     #[test]
-    fn case_insensitive() {
-        let query = "rUsT";
-        let contents = "\
-Rust:
-safe, fast, productive.
-Pick three.
-Trust me.";
-
-        assert_eq!(
-            vec!["Rust:", "Trust me."],
-            search_case_insensitive(query, contents)
-        );
+    fn it_sends_an_over_75_percent_warning_message() {
+        let mock_messenger = MockMessenger::new();
+        let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
+        limit_tracker.set_value(80);
+        assert_eq!(mock_messenger.sent_messages.borrow().len(), 1);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -69,15 +69,4 @@
 //     println!("CustomerSmartPointers created");
 // }
 
-enum List {
-    Cons(i32,Rc<List>),
-    Nil
-}
-use std::rc::Rc;
-
-use crate::List::{Cons,Nil};
-fn main() {
-    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
-    let b = Cons(3, Rc::clone(&a));
-    let c = Cons(4, Rc::clone(&a));
-}
+fn main() {}

8c833b1d8e175f900f1d8e7cadaf887c1cbd044a
the naveen sharma <naveenchaurasiya1501@gmail.com>
2024-09-07 13:03:37 +0530

    Implicit Deref Coercions with Functions and Methods

--- src/main.rs
+++ src/main.rs
@@ -15,13 +15,36 @@
 //     }
 // }
 
-use crate::List::{Cons, Nil};
+use std::ops::Deref;
 
-enum List {
-    Cons(i32, Box<List>),
-    Nil,
+// fn main() {
+//     let x = 5;
+//     let y = MyBox::new(x);
+//     assert_eq!(5, x);
+//     assert_eq!(5, *y);
+// }
+
+// Defining our own smart pointer
+
+struct MyBox<T>(T);
+impl<T> Deref for  MyBox<T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+    
 }
-fn main() {
-    // Box is a smart pointer which implements Deref and Drop trait which help to reference the value and in clean up resp.
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+impl <T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+fn hello(name:&str){
+    print!("Hello, {name}");
+}
+
+fn main(){
+    let m= MyBox::new(String::from("Rusht"));
+    hello(&m);
 }

03081820abe6047c8c1bc83093211ae620c4daf1
the naveen sharma <naveenchaurasiya1501@gmail.com>
2024-09-14 12:40:01 +0530

    spawn thread with and without join

--- src/main.rs
+++ src/main.rs
@@ -163,3 +163,20 @@
 //         Rc::weak_count(&leaf),
 //     );
 // }
+
+use std::{thread, time::Duration};
+
+fn main() {
+let handle=    thread::spawn(|| {
+        for i in 1..10 {
+            println!("Hi number {i} from the spawn thread!");
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+    handle.join().unwrap();
+
+    for i in 1..5 {
+        println!("Hi number {i} from the main thread!");
+        thread::sleep(Duration::from_millis(1));
+    }
+}

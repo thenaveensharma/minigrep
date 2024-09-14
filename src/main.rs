0cf446b495ee874706eecd7e5e9d13e81224c4e5
the naveen sharma <naveenchaurasiya1501@gmail.com>
2024-09-14 14:22:16 +0530

    multiple values using mpsc channel

--- src/main.rs
+++ src/main.rs
@@ -164,7 +164,7 @@
 //     );
 // }
 
-use std::thread;
+// use std::{collections::btree_map::Values, sync::mpsc, thread};
 
 // fn main() {
 // let handle=    thread::spawn(|| {
@@ -181,10 +181,47 @@ use std::thread;
 //     }
 // }
 
+// fn main() {
+//     let vector = vec![1, 2, 3, 4];
+//     let handle = thread::spawn(move || {
+//         print!("{vector:?}");
+//     });
+//     handle.join().unwrap();
+// }
+
+// fn main(){
+//     let (tx,rx)=mpsc::channel();
+//     thread::spawn(move||{
+//         let val=String::from("Naveen");
+//         tx.send(val).unwrap();
+//         println!("val is {val}");
+//     });
+//     let received=rx.recv().unwrap();
+//     println!("Got: {received}");
+// }
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    let vector = vec![1, 2, 3, 4];
-    let handle = thread::spawn(move || {
-        print!("{vector:?}");
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("naveen"),
+            String::from("sharma"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
     });
-    handle.join().unwrap();
+
+    for received in rx {
+        println!("Got: {received}");
+    }
 }

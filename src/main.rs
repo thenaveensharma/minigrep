2b8cde5c77ab50360217eb1d8957ef7e83ade2b0
the naveen sharma <naveenchaurasiya1501@gmail.com>
2024-09-24 23:16:28 +0530

    DST

--- src/main.rs
+++ src/main.rs
@@ -302,56 +302,211 @@
 //     // assert_eq!("I ate a salad for lunch today", post.content());
 // }
 
+// fn main() {
+// let favourite_color: Option<&str> = None;
+// let is_tuesday = false;
+// let age: Result<u8, _> = "34".parse();
+
+// if let Some(color) = favourite_color {
+//     println!("Using your favorite color, {color}, as the background");
+// } else if is_tuesday {
+//     println!("Tuesday is green day!");
+// } else if let Ok(age) = age {
+//     if age > 30 {
+//         println!("Using purple as the background color");
+//     } else {
+//         println!("Using orange as the background color");
+//     }
+// } else {
+//     println!("Using blue as the background color");
+// }
+// let x = 1;
+// match x {
+//     1..=5 => println!("{x}"),
+//     _ => println!("Everything"),
+// }
+// let x = Some(5);
+// let y = 10;
+
+// match x {
+//     Some(50) => println!("Got 50"),
+//     Some(y) => println!("Matched, y = {y}"),
+//     _ => println!("Default case, x = {x:?}"),
+// }
+
+// println!("at the end: x = {x:?}, y = {y}");
+
+// let x=1;
+// match x {
+//     1|2 =>println!("one or two"),
+//     _=> println!("other")
+// }
+// enum Message {
+//     Hello { id: i32 },
+// }
+// let msg = Message::Hello { id: 5 };
+// match msg {
+//     Message::Hello {
+//         id: id_variable @ 3..=7,
+//     } => println!(
+//         "
+//     Found an id in range: {id_variable}"
+//     ),
+//     Message::Hello { id } => print!("Found some other id: {id}"),
+// }
+// }
+
+use std::slice;
+
+// fn main() {
+//     // learn unsafe rust
+
+//     // ----------Superpowers hold by unsafe rust-------------
+
+//     // Dereference a raw pointer
+//     // Call an unsafe function or method
+//     // Access or modify a mutable static variable
+//     // Implement an unsafe trait
+//     // Access fields of a union
+
+//     // let mut num = 5;
+//     // let r1 = &num as *const i32;
+//     // let r2 = &mut num as *mut i32;
+//     // unsafe {
+//     //     print!("{:?},{:?}",*r1,*r2);
+//     //     dangerous();
+//     // }
+
+//     let mut v=vec![1,2,3,4,5,6];
+//     let r=&mut v[..];
+//     let (a,b)=r.split_at_mut(3);
+//     assert_eq!(a, &mut [1, 2, 3]);
+//     assert_eq!(b, &mut [4, 5, 6]);
+// }
+
+// unsafe fn dangerous(){}
+
+// fn split_at_mut(values:&mut [i32],mid:usize)->(&mut [i32],&mut [i32]){
+//     let len=values.len();
+//     let ptr=values.as_mut_ptr();
+//     assert!(mid<=len);
+//     unsafe {
+//         (
+//             slice::from_raw_parts_mut(ptr, mid),
+//             slice::from_raw_parts_mut(ptr.add(mid), len-mid)
+//         )
+//     }
+// }
+
+use std::ops::Add;
+
+// fn main() {
+//     assert_eq!(
+//         Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
+//         Point { x: 3, y: 3 }
+//     );
+// }
+
+// #[derive(Debug, Copy, Clone, PartialEq)]
+// struct Point {
+//     x: i32,
+//     y: i32,
+// }
+// impl Add for Point {
+//     type Output = Point;
+//     fn add(self, other: Point) -> Point {
+//         Point {
+//             x: self.x + other.x,
+//             y: self.y + other.y,
+//         }
+//     }
+// }
+
+trait Pilot {
+    fn fly(&self);
+}
+
+trait Wizard {
+    fn fly(&self);
+}
+
+struct Human;
+
+impl Pilot for Human {
+    fn fly(&self) {
+        println!("This is your captain speaking.");
+    }
+}
+
+impl Wizard for Human {
+    fn fly(&self) {
+        println!("Up!");
+    }
+}
+
+impl Human {
+    fn fly(&self) {
+        println!("*waving arms furiously*");
+    }
+}
+
+trait Animal {
+    fn baby_name() {
+        print!("Animal")
+    }
+}
+struct Dog;
+impl Animal for Dog {
+    fn baby_name() {
+        print!("Puppy");
+    }
+}
 fn main() {
-    // let favourite_color: Option<&str> = None;
-    // let is_tuesday = false;
-    // let age: Result<u8, _> = "34".parse();
+    let person = Human;
+    person.fly();
+    Wizard::fly(&person);
+    Pilot::fly(&person);
 
-    // if let Some(color) = favourite_color {
-    //     println!("Using your favorite color, {color}, as the background");
-    // } else if is_tuesday {
-    //     println!("Tuesday is green day!");
-    // } else if let Ok(age) = age {
-    //     if age > 30 {
-    //         println!("Using purple as the background color");
-    //     } else {
-    //         println!("Using orange as the background color");
-    //     }
-    // } else {
-    //     println!("Using blue as the background color");
-    // }
-    // let x = 1;
-    // match x {
-    //     1..=5 => println!("{x}"),
-    //     _ => println!("Everything"),
-    // }
-    // let x = Some(5);
-    // let y = 10;
+    Dog::baby_name();
+    <Dog as Animal>::baby_name();
+    let p = Point { x: 5, y: 10 };
+    println!("point = {p}");
+    let w = Wrapper(vec![String::from("hello"), String::from("world")]);
+    println!("w = {w}");
+}
 
-    // match x {
-    //     Some(50) => println!("Got 50"),
-    //     Some(y) => println!("Matched, y = {y}"),
-    //     _ => println!("Default case, x = {x:?}"),
-    // }
+use std::fmt::{self, write};
 
-    // println!("at the end: x = {x:?}, y = {y}");
-
-    // let x=1;
-    // match x {
-    //     1|2 =>println!("one or two"),
-    //     _=> println!("other")
-    // }
-    enum Message {
-        Hello { id: i32 },
+trait OutlinePrint: fmt::Display {
+    fn outline_print(&self) {
+        let output = self.to_string();
+        let len = output.len();
+        println!("{}", "*".repeat(len + 4));
+        println!("*{}*", " ".repeat(len + 2));
+        println!("* {output} *");
+        println!("*{}*", " ".repeat(len + 2));
+        println!("{}", "*".repeat(len + 4));
     }
-    let msg = Message::Hello { id: 5 };
-    match msg {
-        Message::Hello {
-            id: id_variable @ 3..=7,
-        } => println!(
-            "
-        Found an id in range: {id_variable}"
-        ),
-        Message::Hello { id } => print!("Found some other id: {id}"),
+}
+
+struct Point {
+    x: i32,
+    y: i32,
+}
+impl fmt::Display for Point {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
     }
+}
+
+struct Wrapper(Vec<String>);
+
+impl fmt::Display for Wrapper {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "[{}]", self.0.join(", "))
+    }
+}
+
+fn generic<T: ?Sized>(t: &T) {
+    
 }
